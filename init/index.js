6f031f346662f2f41fb6ce709db468d83913685c
Davan <davanhs470@gmail.com>
2025-05-28 23:14:49 +0530

    navbar changes

--- init/index.js
+++ init/index.js
@@ -1,8 +1,11 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const mongoUrl = "mongodb://127.0.0.1:27017/majorProject1";
+const mongoUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => console.log("Database is working"))
@@ -14,7 +17,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: '6811a4176f197ed2817df3fb' }))
+    initData.data = initData.data.map((obj) => ({...obj, owner: '6818c998919f33d2d0f68cd2' }))
     await Listing.insertMany(initData.data);
     console.log("Data was initalised");
 };
